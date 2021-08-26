25c70eaade67bc26d1a407611f6030bff8be349b
John DongHyun Kim <zapping0320@gmail.com>
2021-08-26 16:17:02 +0900

    UI 구성

--- ShareTextImage/ViewController.swift
+++ ShareTextImage/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textfieldForShare: UITextField!
+    
+    
+    @IBOutlet weak var imageForShare: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func selectImage(_ sender: Any) {
+    }
+    
 }
 
